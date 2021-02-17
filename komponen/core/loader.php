0fc27b846f3097b73b20e30894b092132b9324aa
andrepradika <andrepradika@gmail.com>
2021-02-17 09:19:28 +0700

    update

---
+++ komponen/core/loader.php
@@ -0,0 +1,186 @@
+
+
+<!-- loader start -->
+<div class="loader_skeleton">
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="main-menu">
+                        <div class="menu-left">
+                            <div class="navbar">
+                                <a href="javascript:void(0)">
+                                    <div class="bar-style"><i class="fa fa-bars sidebar-bar" aria-hidden="true"></i>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="brand-logo">
+                                <a href="index-2.html"><img src="assets/images/icon/logo.png"
+                                    class="img-fluid blur-up lazyload" alt=""></a>
+                            </div>
+                        </div>
+                        <div class="menu-right pull-right">
+                            <div>
+                                <nav>
+                                    <div class="toggle-nav"><i class="fa fa-bars sidebar-bar"></i></div>
+                                    <ul class="sm pixelstrap sm-horizontal">
+                                        <li>
+                                            <div class="mobile-back text-right">Back<i
+                                                class="fa fa-angle-right pl-2" aria-hidden="true"></i></div>
+                                        </li>
+                                        <li><a href="#">pages</a></li>
+                                        <li>
+                                            <a href="#" style="padding-right: 0;" onclick="openSearch()">Cari Produk</a>
+                                            <div id="search-overlay" class="search-overlay">
+                                                <div>
+                                                    <span class="closebtn" onclick="closeSearch()"
+                                                        title="Close Overlay">×</span>
+                                                    <div class="overlay-content">
+                                                        <div class="container">
+                                                            <div class="row">
+                                                                <div class="col-xl-12">
+                                                                    <form>
+                                                                        <div class="form-group">
+                                                                            <input type="text" class="form-control"
+                                                                                id="exampleInputPassword1"
+                                                                                placeholder="Search a Product">
+                                                                        </div>
+                                                                        <button type="submit" class="btn btn-primary"><i
+                                                                            class="fa fa-search"></i></button>
+                                                                    </form>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="home-slider">
+        <div class="home"></div>
+    </div>
+    <section class="collection-banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="ldr-bg">
+                        <div class="contain-banner">
+                            <div>
+                                <h4></h4>
+                                <h2></h2>
+                                <h6></h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="ldr-bg">
+                        <div class="contain-banner">
+                            <div>
+                                <h4></h4>
+                                <h2></h2>
+                                <h6></h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container section-b-space">
+        <div class="row section-t-space">
+            <div class="col-lg-6 offset-lg-3">
+                <div class="product-para">
+                    <p class="first"></p>
+                    <p class="second"></p>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="no-slider row">
+                    <div class="product-box">
+                        <div class="img-wrapper"></div>
+                        <div class="product-detail">
+                            <h4></h4>
+                            <h5></h5>
+                            <h5 class="second"></h5>
+                            <h6></h6>
+                        </div>
+                    </div>
+                    <div class="product-box">
+                        <div class="img-wrapper"></div>
+                        <div class="product-detail">
+                            <h4></h4>
+                            <h5></h5>
+                            <h5 class="second"></h5>
+                            <h6></h6>
+                        </div>
+                    </div>
+                    <div class="product-box">
+                        <div class="img-wrapper"></div>
+                        <div class="product-detail">
+                            <h4></h4>
+                            <h5></h5>
+                            <h5 class="second"></h5>
+                            <h6></h6>
+                        </div>
+                    </div>
+                    <div class="product-box">
+                        <div class="img-wrapper"></div>
+                        <div class="product-detail">
+                            <h4></h4>
+                            <h5></h5>
+                            <h5 class="second"></h5>
+                            <h6></h6>
+                        </div>
+                    </div>
+                    <div class="product-box">
+                        <div class="img-wrapper"></div>
+                        <div class="product-detail">
+                            <h4></h4>
+                            <h5></h5>
+                            <h5 class="second"></h5>
+                            <h6></h6>
+                        </div>
+                    </div>
+                    <div class="product-box">
+                        <div class="img-wrapper"></div>
+                        <div class="product-detail">
+                            <h4></h4>
+                            <h5></h5>
+                            <h5 class="second"></h5>
+                            <h6></h6>
+                        </div>
+                    </div>
+                    <div class="product-box">
+                        <div class="img-wrapper"></div>
+                        <div class="product-detail">
+                            <h4></h4>
+                            <h5></h5>
+                            <h5 class="second"></h5>
+                            <h6></h6>
+                        </div>
+                    </div>
+                    <div class="product-box">
+                        <div class="img-wrapper"></div>
+                        <div class="product-detail">
+                            <h4></h4>
+                            <h5></h5>
+                            <h5 class="second"></h5>
+                            <h6></h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- loader end -->
+
